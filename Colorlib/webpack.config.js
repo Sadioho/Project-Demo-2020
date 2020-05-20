0ee5e39e3a8a2efcb2a7f348ec95d9f7f8885f5b
Sadioho <hoxuananh9.3@gmail.com>
2020-05-20 21:54:49 +0700

    update menu mobile

--- Colorlib/webpack.config.js
+++ Colorlib/webpack.config.js
@@ -80,6 +80,18 @@ module.exports = {
                },
             ],
          },
+         // {
+         //    test: /\.html$/,
+         //    use: [
+         //       {
+         //          loader: 'file-loader',
+         //          options: {
+         //             name: '[name].[ext]',
+         //          },
+         //       }
+         //    ],
+         //    exclude : path.resolve(__dirname, 'src/index.html')
+         // },
       ],
    },
 };
